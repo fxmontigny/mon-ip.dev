9d9cef93ed1c67ff7df186a4e4c140539e3417c3
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2019-09-02 16:39:25 +0200

    add simple ip

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-// server.js
 var express = require('express');
-var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+var morgan = require('morgan');
+
+const app = express();
 app.use(serveStatic(__dirname + '/dist'));
+app.use(morgan('combined'));
+
+app.get('/ip', function(req, res) {
+  res.json({ ipClient: req.connection.remoteAddress });
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started ' + port);
+
+console.log('server started ', port);
