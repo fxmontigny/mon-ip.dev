b6979ad57cfdd956c9ea6b667366eb785425d526
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2019-09-02 16:54:25 +0200

    change ip protocol

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const app = express();
 app.use(serveStatic(__dirname + '/dist'));
 app.use(morgan('combined'));
 
-app.get('/ip', function(req, res) {
-  res.json({ ipClient: req.connection.remoteAddress });
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port);
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,22 +7,61 @@
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
   name: "HelloWorld",
   data() {
     return { ip: null };
   },
   methods: {
-    loadIp() {
-      axios.get("/ip").then(response => {
-        this.ip = response.data.ipClient;
+    getUserIP(onNewIP) {
+      //  onNewIp - your listener function for new IPs
+      //compatibility for firefox and chrome
+      var myPeerConnection =
+        window.RTCPeerConnection ||
+        window.mozRTCPeerConnection ||
+        window.webkitRTCPeerConnection;
+      var pc = new myPeerConnection({
+          iceServers: []
+        }),
+        noop = function() {},
+        localIPs = {},
+        ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+
+      function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+      }
+
+      //create a bogus data channel
+      pc.createDataChannel("");
+
+      // create offer and set local description
+      pc.createOffer().then(function(sdp) {
+        sdp.sdp.split("\n").forEach(function(line) {
+          if (line.indexOf("candidate") < 0) return;
+          line.match(ipRegex).forEach(iterateIP);
+        });
+
+        pc.setLocalDescription(sdp, noop, noop);
       });
+
+      //listen for candidate events
+      pc.onicecandidate = function(ice) {
+        if (
+          !ice ||
+          !ice.candidate ||
+          !ice.candidate.candidate ||
+          !ice.candidate.candidate.match(ipRegex)
+        )
+          return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+      };
     }
   },
   mounted() {
-    this.loadIp();
+    this.getUserIP(ip => {
+      this.ip = ip;
+    });
   }
 };
 </script>
